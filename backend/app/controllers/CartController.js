abde390c1eafa9659d62493dd30a3cee7733876d
Dinh <23020043@vnu.edu.vn>
2025-04-04 23:02:33 +0700

    add JsDocs

--- backend/app/controllers/CartController.js
+++ backend/app/controllers/CartController.js
@@ -7,6 +7,15 @@ const {
 } = require('../../utils/mongoose');
 
 class CartController {
+
+    /**
+     * Add product to cart.
+     * @param {*} req 
+     * - Lấy dữ liệu req.user thông qua middleware.
+     * - Truyền vào req.body thông tin sản phẩm { productId, quantity }
+     * @param {*} res 
+     * @param {*} next 
+     */
     addToCart(req, res, next) {
         cartService
             .addToCart(req.user.id, req.body)
@@ -19,6 +28,14 @@ class CartController {
             .catch(next);
     }
 
+    /**
+     * Update product in cart.
+     * @param {*} req 
+     * - Lấy dữ liệu req.user thông qua middleware.
+     * - Truyền vào req.body thông tin sản phẩm { productId, quantity }
+     * @param {*} res 
+     * @param {*} next 
+     */
     updateCart(req, res, next) {
         cartService
             .updateCart(req.user.id, req.body)
