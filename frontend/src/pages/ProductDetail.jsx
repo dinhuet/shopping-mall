d0268987ed8c14b105c4937d25ea95741ecae6cd
Phạm Tùng Lâm <tunglamp227@gmail.com>
2025-04-15 21:06:26 +0700

    Show details

---
+++ frontend/src/pages/ProductDetail.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import productAPI from '../api/productAPI';
+import { useCart } from '../context/CartContext';
+
+const ProductDetail = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const { addToCart } = useCart();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await productAPI.getById(id);
+        setProduct(res.data);
+      } catch (err) {
+        console.error('Lỗi khi lấy thông tin sản phẩm:', err.response?.data?.message);
+      }
+    };
+    fetchProduct();
+  }, [id]);
+
+  if (!product) return <p>Đang tải...</p>;
+
+  return (
+    <div>
+      <img src={product.image} alt={product.name} />
+      <h2>{product.name}</h2>
+      <p>Giá: {product.price}₫</p>
+      <p>{product.description}</p>
+      <button onClick={() => addToCart(product._id)}>Thêm vào giỏ hàng</button>
+    </div>
+  );
+};
+
+export default ProductDetail;
