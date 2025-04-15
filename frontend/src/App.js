91cd66adf6701c169af8d3fd910f38c79239eecf
Phạm Tùng Lâm <tunglamp227@gmail.com>
2025-04-15 21:07:20 +0700

    Wrap app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,34 @@ import Register from './pages/Register';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
-import { CartProvider } from './context/CartContext'; 
+import { CartProvider } from './context/CartContext';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <CartProvider> {/* ✅ Bọc toàn bộ ứng dụng */}
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/cart"
+              element={
+                <ProtectedRoute>
+                  <Cart />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
