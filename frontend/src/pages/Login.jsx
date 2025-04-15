2c85ca4f70a1ab9e9a781b1f369b8e4c76045f7e
Phạm Tùng Lâm <tunglamp227@gmail.com>
2025-04-15 13:46:37 +0700

    create LoginPage

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import authAPI from '../api/authAPI';
+
+const handleLogin = async () => {
+  try {
+    const res = await authAPI.login({ email, password });
+    const { token, user } = res.data;
+
+    localStorage.setItem('token', token);
+    // redirect hoặc set user context
+  } catch (error) {
+    console.error('Login failed:', error.response.data.message);
+  }
+};
